eff6d8d9670aecd9ef8ea1d4f0c394b723b4aee3
Matias Godoy <matiasgodoy@Matiass-Mac-mini.local>
2023-07-19 22:15:52 -0400

    base proyecto

---
+++ lib/features/auth/presentation/providers/login_form_provider.dart
@@ -0,0 +1,18 @@
+import 'package:teslo_shop/features/shared/infrastructure/inputs/inputs.dart';
+
+
+//! 1- state del provider
+class LoginFormState {
+  final bool isPosting;
+  final bool isFormPosted;
+  final bool isValid;
+  final Email email;
+  final Password password;
+
+  LoginFormState(
+      {this.isPosting = false,
+      this.isFormPosted = false,
+      this.isValid = false,
+      this.email = const Email.pure(),
+      this.password = const Password.pure()});
+}
